f9d908381224ef7afc7f469d8a792f95a1a08811
YawEnLiuu <yukinaryxd@gmail.com>
2026-06-04 19:39:41 -0400

    Enable CORS for deployed frontend

--- back_end/app.py
+++ back_end/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from src.pipeline import run_pipeline, DATA_DIR
 from src.resume_parser import extract_text_from_pdf
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
